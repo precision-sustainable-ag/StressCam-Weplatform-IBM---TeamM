8f5a04ec56476432922b4ed4e01b4bb48fd6d405
Qingwei Cheng <cqw1043936513@gmail.com>
2021-02-07 17:30:52 -0500

    updated modular codebase

--- Software Files/Raspberry Pi Code/resize_image.py
+++
@@ -1,7 +0,0 @@
-def resize_image(height, width):
-    image_size = {
-        "image_Height" : int(cmd.data['Height'])
-        "image_Width" : int(cmd.data['Width'])
-    }
-        print("Images Resized to", imageWidth,"x", imageHeight)
-        return imageHeight, imageWidth
