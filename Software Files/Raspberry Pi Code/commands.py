a20e19a0e25f105bb82ae89a7657f06127463309
Qingwei Cheng <cqw1043936513@gmail.com>
2021-02-11 10:10:00 -0500

    modify

--- Software Files/Raspberry Pi Code/commands.py
+++ Software Files/Raspberry Pi Code/commands.py
@@ -3,6 +3,8 @@ import datetime
 import time
 from time import sleep
 from subprocess import run
+from gpiozero import CPUTemperature
+
 
 init_values = {
     "imageWidth" : 640,
@@ -27,12 +29,11 @@ def capture_image():
     currDate = datetime.datetime.now().strftime("%d_%m_%y") #Set variable currDate to the datetime the image is taken
     currTime = datetime.datetime.now().strftime("%H_%M_%S") #set variable currTime to the time the image is taken
     print("capturing image")
-    camera.capture('/home/pi/images/' + currDate + '_' + currTime + init_values.get("imageFormat")) #name the image in the format of Y-M-D-H-m-s in jpg, bmp, png
+    camera.capture('/home/pi/images/' + currDate + '_' + currTime + init_values.get("imageFormat")) #name the image in the format of Y-M-D-h-m-s in jpg, bmp, png
     camera.close() # turn off camera to release the camera resourse
     #camera.stop_preview()
     print("Image Taken")
     sleep(5)
-
 
 
 def resize_image(height = init_values.get("imageHeight"), width = init_values.get("imageWidth")):
@@ -43,7 +44,7 @@ def resize_image(height = init_values.get("imageHeight"), width = init_values.ge
     print("Images Resized to", image_size.get("image_Width"),"x", image_size.get("image_Height"),"y")
     return image_size
 
-def change_resolution(new_x_resolution,new_y_resolution):
+def change_resolution(new_x_resolution = 2592,new_y_resolution = 1944):
     new_resolution={
     "imageResolutionX" : new_x_resolution,
     "imageResolutionY" : new_y_resolution
@@ -63,4 +64,55 @@ def run_script(script_type):
             run(['python3', '/home/pi'+ script_type])
         else:
             run(['sudo','sh', '/home/pi'+ script_type])
+
+def change_schedule(start_time, end_time):
+    startTime = datetime.datetime.strptime(start_time,"%H:%M") # parse start time
+    endTime = datetime.datetime.strptime(end_time,"%H:%M") # parse end time
+    onTimeHours = endTime.hour - startTime.hour # calculate how many hours are on
+    onTimeMin = endTime.minute - startTime.minute # calcualte how many minus are on
+    #calculate off time hours and minuits
+    # if minute vlaue is larger than zero, hour value need to be offset by 1.
+    if(onTimeMin>0):
+        offTimeHours = 23 - onTimeHours
+        offTimeMin = 60- onTimeMin
+    elif(onTimeMin == 0):
+        offTimeHours = 24-onTimeHours
+        offTimeMin = 0
+    ## This is consfusing, the value of 'onTimeMin' an be smaller than 0?
+    else: ## seems wrong
+        offTimeHours = 24 - onTimeHours
+        onTimeHours = onTimeHours-1
+        onTimeMin = 60 - abs(onTimeMin)
+        offTimeMin = 60 - onTimeMin
+    # modify witttpi schedule
+    f =  open('/home/pi/wittyPi/schedules/schedule.wpi','w')
+    f.write('BEGIN   2015-08-01 '+ start_time+':00'+ '\n')
+    f.write('END     2025-07-31 23:59:59'+ '\n')
+    f.write('ON      H'+ str(onTimeHours) + ' M' + str(onTimeMin)+ '\n')
+    f.write('OFF     H'+ str(offTimeHours)+ ' M' + str(offTimeMin))
+    f.close
+
+def change_image_format(image_format = '.jpg'):
+    if(image_format == '.jpg'):
+        print("Format changed to:"+image_format)
+    elif(image_format == '.png'):
+        print("Format changed to:"+image_format)
+    elif(image_format == '.bmp'):
+        print("Format changed to:"+image_format)
+    else:
+        print("Incompatible format")
+    init_values["imageFormat"] = image_format
+    return init_values.get("imageFormat")
+
+
+class Device:
+    def __init__(self, device_ID, device_status, latitude, Longtitude, water_stress_level = 0, cpu_temperature = CPUTemperature, date_1 = datetime.datetime.now().strftime("%d_%m_%y"), time_1 = datetime.datetime.now().strftime("%H_%M_%S")):
+        self.DEVICE_ID = device_ID
+        self.DEVICE_STATUS = device_status
+        self.LATITUDE = latitude
+        self.LONGITUDE = Longtitude
+        self.WATER_STRESS_LEVEL = water_stress_level
+        self.CPU_TEMPERATURE = cpu_temperature
+        self.DATE_1 = date_1
+        self.TIME_1 = time_1
 
